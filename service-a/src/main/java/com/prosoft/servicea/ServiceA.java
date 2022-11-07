9dae167a96df2563c2bab13c305c3274e5d20ef8
sproshchaev <sproshchaev@gmail.com>
2022-11-07 21:45:36 +0500

    Added dependencies service-a

--- service-a/src/main/java/com/prosoft/servicea/ServiceA.java
+++ service-a/src/main/java/com/prosoft/servicea/ServiceA.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceAApplication {
+public class ServiceA {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceAApplication.class, args);
+		SpringApplication.run(ServiceA.class, args);
 	}
 
 }
--- service-a/src/test/java/com/prosoft/servicea/ServiceATests.java
+++ service-a/src/test/java/com/prosoft/servicea/ServiceATests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServiceAApplicationTests {
+class ServiceATests {
 
 	@Test
 	void contextLoads() {
