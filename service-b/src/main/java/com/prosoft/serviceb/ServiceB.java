2329a27648907f8988c07e7635be41e4b7640b9f
sproshchaev <sproshchaev@gmail.com>
2022-11-07 21:50:11 +0500

    Updated service-b

--- service-b/src/main/java/com/prosoft/serviceb/ServiceB.java
+++ service-b/src/main/java/com/prosoft/serviceb/ServiceB.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceBApplication {
+public class ServiceB {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceBApplication.class, args);
+		SpringApplication.run(ServiceB.class, args);
 	}
 
 }
--- service-b/src/test/java/com/prosoft/serviceb/ServiceBTests.java
+++ service-b/src/test/java/com/prosoft/serviceb/ServiceBTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServiceBApplicationTests {
+class ServiceBTests {
 
 	@Test
 	void contextLoads() {
